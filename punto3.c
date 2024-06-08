3506b3176873a6264eaabab988d336f59ab6a3d2
Benja <benjaminvilla409@gmail.com>
2024-06-08 20:21:16 -0300

    Modifique el código del ejercicio 3

--- punto3.c
+++ punto3.c
@@ -7,19 +7,21 @@ void main()
 {
     char *nombres[CANT];
     char *Buff = (char *)malloc(100 * sizeof(char));
-
+    printf("Ingrese 5 nombres:\n");
     // Cargo el nombre
     for (int i = 0; i < CANT; i++)
     {
+        printf("%d. ",i+1);
         gets(Buff);
         nombres[i] = (char *)malloc((strlen(Buff) + 1) * sizeof(char));
         strcpy(nombres[i], Buff);
-        // Printeo el nombre
-        puts(nombres[i]);
     }
+    //libero, printeo y libero.
     free(Buff);
+    puts("\nNombres ingresados:");
     for (int i = 0; i < CANT; i++)
     {
+        printf("%d. %s\n",i+1,nombres[i]);
         free(nombres[i]);
     }
 }
